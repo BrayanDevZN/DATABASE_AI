46c577d9ecbed1f310783f85c509a487711d07a4
brayan.ds <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>
2026-05-25 16:17:57 -0300

    v7.5(debug env pass

--- api/routes.py
+++ api/routes.py
@@ -39,10 +39,9 @@ def login(data: models.Login):
 
 @app.post("/env_pass", status_code=status.HTTP_200_OK)
 def env_pass(data: models.Env_CodePass):
-    if "email" in data.model_dump().keys():
-        return manager.User_Login().Env_codePass(email=data.model_dump()["email"]) 
     
-    return manager.User_Login().Env_codePass(token=data.model_dump()["token"]) 
+    
+    return manager.User_Login().Env_codePass(token=data.model_dump()) 
 
 
 @app.patch("/update_auth_pass", status_code=status.HTTP_200_OK)
